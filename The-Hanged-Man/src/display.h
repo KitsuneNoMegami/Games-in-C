fc5ed1f52624c07210837a253c2c8e0a73660c1a
Elisabeth <e_mops@hotmail.com>
2025-07-28 12:03:17 +0200

    The fist display, the Game Over ofc

---
+++ The-Hanged-Man/src/display.h
@@ -0,0 +1,2 @@
+#pragma once
+#define DEAD "+------+\n|/     |\n|      o\n|     /0\\\n|     / \\ \n|\\\n"
--- The-Hanged-Man/src/main.c
+++ The-Hanged-Man/src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "display.h"
 
 int main(){
+    printf(DEAD);
     return EXIT_SUCCESS;
 }
